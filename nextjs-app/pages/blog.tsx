af73f08577d8774a21642292276667cae53830be
Zack Spellman <zandaleph@gmail.com>
2022-04-09 16:08:25 -0700

    Upgrade to React 18, and other packages too

--- nextjs-app/pages/blog.tsx
+++ nextjs-app/pages/blog.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import type { NextPage } from "next";
 import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 const Home: NextPage = () => {
   return (
@@ -8,16 +9,18 @@ const Home: NextPage = () => {
       <Typography variant="h3" component="h1" gutterBottom>
         Alt⎇⬥Meta
       </Typography>
-      <Typography variant="body1" gutterBottom>
-        People talking without speaking
-        <br />
-        People hearing without listening
-        <br />
-        People writing songs that voices never shared
-        <br />
-        And no one dared disturb the sound of silence
+      <Box>
+        <Typography variant="body1" gutterBottom>
+          People talking without speaking
+          <br />
+          People hearing without listening
+          <br />
+          People writing songs that voices never shared
+          <br />
+          And no one dared disturb the sound of silence
+        </Typography>
         <Typography textAlign="right">- Paul Simon</Typography>
-      </Typography>
+      </Box>
       <Typography variant="body1">
         Blog posts have not been migrated yet, so for now, silence.
       </Typography>
--- nextjs-app/pages/index.tsx
+++ nextjs-app/pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import type { NextPage } from "next";
 import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 const Home: NextPage = () => {
   return (
@@ -8,10 +9,12 @@ const Home: NextPage = () => {
       <Typography variant="h3" component="h1" gutterBottom>
         Alt⎇⬥Meta
       </Typography>
-      <Typography variant="body1" gutterBottom>
-        I think this is the beginning of a beautiful friendship.
+      <Box>
+        <Typography variant="body1" gutterBottom>
+          I think this is the beginning of a beautiful friendship.
+        </Typography>
         <Typography textAlign="right">- Casablanca</Typography>
-      </Typography>
+      </Box>
       <Typography variant="body1">
         Site under construction, more to come later.
       </Typography>
