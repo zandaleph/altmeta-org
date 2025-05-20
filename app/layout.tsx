f6695eace4e5d3f8eb0136d1c988c475d99441c7
Zack Spellman <zandaleph@gmail.com>
2025-05-20 15:36:27 -0700

    feat: create and add a favicon and other icon files

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,14 @@ const sourceCodePro = Source_Code_Pro({
 export const metadata: Metadata = {
   title: "Altmeta.org",
   description: "Personal website and blog of Zack Spellman",
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png" }],
+  },
 };
 
 export default function RootLayout({
