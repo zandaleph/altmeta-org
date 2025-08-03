943c2839f886032b6414265ea2a1d9d04a8b4011
Zack Spellman <zandaleph@gmail.com>
2025-08-03 15:52:39 -0700

    [Bugfix] Fix image sizes

--- app/weblog/zack/[year]/[month]/[slug]/BlogPicture.tsx
+++ app/weblog/zack/[year]/[month]/[slug]/BlogPicture.tsx
@@ -1,21 +1,43 @@
 import Picture from "next-export-optimize-images/picture";
 import { ImgHTMLAttributes } from "react";
+import fs from "fs/promises";
+import sharp from "sharp";
+import path from "path";
 
 interface BlogPictureProps extends ImgHTMLAttributes<HTMLImageElement> {
   year: string;
   month: string;
 }
 
-export default function BlogPicture({
+export default async function BlogPicture({
   src,
   alt,
   year,
   month,
+  ...props
 }: BlogPictureProps) {
-  let absSrc = src as string;
-  if (absSrc.startsWith("./")) {
-    absSrc = `/weblog/zack/${year}/${month}/${absSrc.slice(2)}`;
+  if (!(src instanceof String) || !src.startsWith("./")) {
+    return (
+      <picture>
+        <img {...props} src={src} alt={alt as string} />
+      </picture>
+    );
   }
+  const rootSrc = `/weblog/zack/${year}/${month}/${src.slice(2)}`;
 
-  return <Picture src={absSrc} alt={alt as string} width={1200} height={200} />;
+  const projectRoot = process.cwd();
+  const filePath = path.join(projectRoot, "public", rootSrc);
+
+  const file = await fs.readFile(filePath);
+  const image = await sharp(file).metadata();
+
+  return (
+    <Picture
+      {...props}
+      src={rootSrc}
+      alt={alt as string}
+      width={image.width}
+      height={image.height}
+    />
+  );
 }
